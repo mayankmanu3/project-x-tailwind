dcea6c324ff9655b0ebea6e4119396d036aca35a
mayankm03 <mayank.m@doodleblue.com>
2021-04-27 23:39:21 +0530

    tailwind initialize

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="bg-purple-500">
+        <h1 className="text-gray-50 text-left">Project-X</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
